94f9f262dff48eaa931d1539cbeb9474b9d808c1
seungju.kim <pipiru100@gmail.com>
2024-04-28 17:37:09 +0900

    Changes

---
+++ src/tour/Main.kt
@@ -0,0 +1,10 @@
+package tour
+
+//const val name: String = "Kotlin"
+
+fun main(args: Array<String>) {
+    println(args)
+    val name = "MaxMan"
+    val age = 20
+    println("$name is $age years old.")
+}
