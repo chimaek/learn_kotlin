666729bdbc2e3ed30f486d255b1bf5f6b866829e
seungju.kim <pipiru100@gmail.com>
2024-04-28 17:41:49 +0900

    베이직 타입 연습

--- src/tour/Main.kt
+++ src/tour/Main.kt
@@ -8,3 +8,12 @@ fun main(args: Array<String>) {
     val age = 20
     println("$name is $age years old.")
 }
+
+fun basicType() {
+    val a: Int = 1000
+    val b: String = "log message"
+    val c: Double = 3.14
+    val d: Long = 100_000_000_000
+    val e: Boolean = false
+    val f: Char = '\n'
+}
